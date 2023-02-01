e5f5a6b5e9ac03499c5c48e3a803b918d3401bce
Joshua Ching <jchingdev@gmail.com>
2023-01-31 20:24:32 -0800

    business logic to scrape dividend data

---
+++ DividendWallet/Network/WebScraper/WSDividendEventModel.swift
@@ -0,0 +1,13 @@
+//
+//  WSDividendEventModel.swift
+//  Dividend Wallet
+//
+//  Created by Joshua Ching on 1/31/23.
+//
+
+import Foundation
+
+struct WSDividendEventModel {
+    let date: String
+    let amount: Double
+}
