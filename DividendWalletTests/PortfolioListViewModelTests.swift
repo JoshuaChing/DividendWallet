fbffde8580b06279a66751c28082efdfe3f0bd1f
Joshua Ching <jchingdev@gmail.com>
2023-04-25 19:23:10 -0700

    test for list events

--- DividendWalletTests/PortfolioListViewModelTests.swift
+++ DividendWalletTests/PortfolioListViewModelTests.swift
@@ -32,4 +32,69 @@ final class PortfolioListViewModelTests: XCTestCase {
         XCTAssertEqual(sut.portfolioListRowViewModels[2].symbol, "A")
         XCTAssertEqual(sut.portfolioListRowViewModels[2].estimatedAnnualDividendIncome, 1.0)
     }
+
+    func tests_onDividendHistoryUpdated_updateListEventsRowModels() {
+        // Given
+        let mockDividendHistoryModel = createMockDividendHistoryModel()
+        let sut = PortfolioListViewModel()
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels.count, 0)
+
+        // When
+        NotificationCenterManager.postUpdatePositionsDividendHistory(dividendHistory: mockDividendHistoryModel)
+        RunLoop.main.run(mode: .default, before: .distantPast)
+
+        // Then
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels.count, 2)
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[0].symbol, "C")
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[0].lastDividendValue, 2.0)
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[0].shareCount, 5.0)
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[0].estimatedIncome, 10.0)
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[1].symbol, "B")
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[1].lastDividendValue, 3.0)
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[1].shareCount, 6.0)
+        XCTAssertEqual(sut.portfolioListEventsRowViewModels[1].estimatedIncome, 18.0)
+    }
+
+    private func createMockDividendHistoryModel() -> PositionsDividendHistoryModel{
+        let calendar = Calendar.current
+        let currentDate = Date()
+        let currentMonth = calendar.component(.month, from: currentDate)
+
+        // get first day of current month
+        guard let currentMonthDate = calendar.date(from: DateComponents(year: calendar.component(.year, from: currentDate), month: currentMonth, day: 1)) else {
+            fatalError("Could not get the first day of the current month.")
+        }
+
+        // get previous month
+        guard let previousMonthDate = calendar.date(byAdding: DateComponents(month: -1), to: currentDate) else {
+            fatalError("Could not get the previous month.")
+        }
+
+        // get next month
+        guard let nextMonthDate = calendar.date(byAdding: DateComponents(month: 1), to: currentDate) else {
+            fatalError("Could not get the next month.")
+        }
+
+        // get next next month
+        guard let nextNextMonthDate = calendar.date(byAdding: DateComponents(month: 2), to: currentDate) else {
+            fatalError("Could not get the next next month.")
+        }
+
+        let dividendHistoryModel: PositionsDividendHistoryModel = [
+            "A": PositionDividendHistoryModel(events: [WSDividendEventModel(date: previousMonthDate, amount: 1.0)], shareCount: 5),
+            "B": PositionDividendHistoryModel(events: [
+                WSDividendEventModel(date: previousMonthDate, amount: 1.0),
+                WSDividendEventModel(date: currentMonthDate, amount: 3.0), // this dividend should be captured
+                WSDividendEventModel(date: nextNextMonthDate, amount: 1.0)
+            ], shareCount: 6),
+            "C": PositionDividendHistoryModel(events: [
+                WSDividendEventModel(date: previousMonthDate, amount: 1.0),
+                WSDividendEventModel(date: nextMonthDate, amount: 2.0), // this dividend should be captured
+                WSDividendEventModel(date: nextNextMonthDate, amount: 1.0)
+            ], shareCount: 5),
+            "D": PositionDividendHistoryModel(events: [WSDividendEventModel(date: nextNextMonthDate, amount: 1.0)], shareCount: 5),
+        ]
+
+        return dividendHistoryModel
+    }
 }
